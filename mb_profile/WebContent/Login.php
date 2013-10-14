5bf36a288400996bb02f5792670b61154267eaf1
bmagaji2 <begebawa@hotmail.com>
2013-10-14 13:03:24 -0500

    Created the login for user profile

---
+++ mb_profile/WebContent/Login.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="ISO-8859-1">
+<title>Anumushi Nig. LTD</title>
+</head>
+<body>
+<h1>Benivido Welkom Welcome</h1>
+<h2>Login Your Organization:</h2>
+
+
+<form  name="loginForm"  method="post" action="loginE.php">
+<br>
+User Name: <input type="text" name="uname"><br>
+Password: <input type="text" name="pwd"><br><br>
+
+<input type="submit" value = "Login">
+
+</form>
+
+
+<section>
+<p><a href="./index.php">Back to Main Page</a>
+</section>
+
+</body>
+</html>
